72ddfd48021b77ce6e30fc8ec5371d016b9de9b7
Hyungmo Gu <guhyungm7@gmail.com>
2022-04-22 21:33:29 -0600

    Add progress

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,15 +3,21 @@
 
   // Headers mobile navigation menu that shows on click
   $(function () {
-      var nav = $('nav.td-navbar').first();
-      if (!nav) return;
+    var body = $('body');
+    var nav = $('nav.td-navbar').first();
+    var mobileMenuBtn = $(nav).find('.navbar-mobile-button').first();
 
-      var mobileMenuBtn = nav.find('.navbar-mobile-button').first();
-      if (!mobileMenuBtn) return;
+    $(mobileMenuBtn).click(() => {
+      mobileMenuBtn.toggleClass("open");
 
-      $(mobileMenuBtn).click(() => {
-        nav.toggleClass("open");
-      })
+      if (mobileMenuBtn.hasClass("open")) {
+        body.addClass('mobile-menu-open');
+        nav.addClass('mobile-menu-open');
+      } else {
+        body.removeClass('mobile-menu-open');
+        nav.removeClass('mobile-menu-open');
+      }
+    })
   });
 
 }(jQuery));
