f4663bb91b08dc42bc4bace332bf5215fc1638ee
Hyungmo Gu <guhyungm7@gmail.com>
2022-04-23 10:09:12 -0600

    Add progress

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,6 @@
 (function ($) {
   'use strict';
+  var breakpoint = 1200;
 
 
   // Headers mobile navigation menu that shows on click
@@ -19,6 +20,21 @@
         header.removeClass('mobile-menu-open');
       }
     })
+  });
+
+  // Headers mobile navigation menu go back to normal after breakpoint
+  $(function () {
+    var body = $('body');
+    var header = $('header');
+    var mobileMenuBtn = $(header).find('.navbar-mobile-button').first();
+
+    $(window).resize(() => {
+      if ($(document).width() > breakpoint) {
+        body.removeClass('mobile-menu-open');
+        header.removeClass('mobile-menu-open');
+        mobileMenuBtn.removeClass('open');
+      }
+    });
   });
 
 }(jQuery));
