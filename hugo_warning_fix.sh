e6cba1988e4415ac277c1bbeda8504c93779f825
Hyungmo Gu <guhyungm7@gmail.com>
2022-05-26 07:37:09 -0600

    Add progress

--- hugo_warning_fix.sh
+++ hugo_warning_fix.sh
@@ -1,4 +1,13 @@
-sudo launchctl limit maxfiles 65535 200000;
-ulimit -n 65535;
-sudo sysctl -w kern.maxfiles=100000;
-sudo sysctl -w kern.maxfilesperproc=65535;
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sudo launchctl limit maxfiles 65535 200000;
+  ulimit -n 65535;
+  sudo sysctl -w kern.maxfiles=100000;
+  sudo sysctl -w kern.maxfilesperproc=65535;
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  sudo launchctl limit maxfiles 65535 200000;
+  ulimit -n 65535;
+  sudo sysctl -w kern.maxfiles=100000;
+  sudo sysctl -w kern.maxfilesperproc=65535;
+fi
+
+hugo server;
