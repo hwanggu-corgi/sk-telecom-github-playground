695437f698ce7ae8bdc313f5aef2a17b0130f952
Hyungmo Gu <guhyungm7@gmail.com>
2022-05-28 10:37:05 -0600

    Add progress

--- test/test.py
+++ test/test.py
@@ -7,26 +7,22 @@ class TestHugo(unittest.TestCase):
   def setUp(self):
     self.dirPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     self.executable = "start.sh"
-    self.proc = subprocess.Popen(["sh", os.path.join(self.dirPath, self.executable)])
-
-  def tearDown(self):
-    # because sigkill is bad
-    self.proc.terminate()
 
   def test_start_command_should_not_return_error(self):
     expected = True
     result = True
 
     try:
-      subprocess.check_call(["sh", os.path.join(self.dirPath, self.executable)])
+      output = subprocess.run(["sh", os.path.join(self.dirPath, self.executable)], timeout=10, check=True)
+      print(output)
     except subprocess.CalledProcessError:
       result = False
+    except subprocess.TimeoutExpired:
+
 
     self.assertEqual(result, expected)
 
   def test_start_command_should_show_web_server_is_starting (self):
-    for line in self.proc.stdout:
-      print(line)
     pass
 
 if __name__ == '__main__':
