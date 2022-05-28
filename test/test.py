0ff33a0e8db9b0dd8712c8cf81de46e5dab71dc6
Hyungmo Gu <guhyungm7@gmail.com>
2022-05-28 10:10:47 -0600

    Add progress

--- test/test.py
+++ test/test.py
@@ -7,25 +7,27 @@ class TestHugo(unittest.TestCase):
   def setUp(self):
     self.dirPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     self.executable = "start.sh"
-    self.proc = subprocess.Popen(["sh", os.path.join(self.dirPath, self.executable)], stdout=subprocess.PIPE)
+    self.proc = subprocess.Popen(["sh", os.path.join(self.dirPath, self.executable)])
 
-  def test_hugo_start(self):
-    # checks mac Montesery
+  def tearDown(self):
+    # because sigkill is bad
+    self.proc.terminate()
 
+  def test_start_command_should_not_return_error(self):
     expected = True
     result = True
 
-    # Run docker container and setup macOS
-
     try:
-
-    except subprocess.CalledProcessError as e:
+      subprocess.check_call(["sh", os.path.join(self.dirPath, self.executable)])
+    except subprocess.CalledProcessError:
       result = False
 
     self.assertEqual(result, expected)
 
-  def tearDown(self):
-    self.proc.terminate()
+  def test_start_command_should_show_web_server_is_starting (self):
+    for line in self.proc.stdout:
+      print(line)
+    pass
 
 if __name__ == '__main__':
     unittest.main()
